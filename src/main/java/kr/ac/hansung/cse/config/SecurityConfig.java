17151d8dd1b219190eb6bc28bc80adb44cff0b23
sycho <sycho0311@gmail.com>
2018-06-04 17:40:45 +0900

    helloSpringBoot
    logging & security

---
+++ src/main/java/kr/ac/hansung/cse/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package kr.ac.hansung.cse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration // java class를 통해 configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest()
+				.permitAll()
+				.and()
+			.csrf().disable();
+	}
+	
+}
---
+++ src/main/java/kr/ac/hansung/cse/controller/HomeController.java
@@ -0,0 +1,25 @@
+package kr.ac.hansung.cse.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	// @GetMapping is a composed annotation that acts as a shortcut for
+	// @RequestMapping(value="/", method = RequestMethod.GET)
+	@GetMapping("/")
+	public String home(Model model) {
+		
+		logger.debug("Calling home()");
+		
+		model.addAttribute("message", "hello world");
+		return "index";
+		
+	}
+}
